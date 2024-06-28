ec7bf37f9f34144bd7e210d53d70da34e7aedecc
Youcef <h.youcef6346@gmail.com>
2024-06-28 21:47:20 +0100

    Named each route for tests

--- routes/api.php
+++ routes/api.php
@@ -5,25 +5,25 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TaskController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login',    [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login',    [AuthController::class, 'login'])->name('login');
 
 
 Route::middleware('auth:sanctum')->group(function() {
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
     // check user if connected
-    Route::get('/user', fn(Request $request) => $request->user());
+    Route::get('/user', fn(Request $request) => $request->user())->name('checkUser');
 
     Route::middleware('roles:admin')->group(function () {
-        Route::get('/tasks/deleted', [TaskController::class, 'deleted']);
+        Route::get('/tasks/deleted', [TaskController::class, 'deleted'])->name('deleted');
     });
     
     Route::middleware('roles:admin,user')->group(function () {
-        Route::get('/tasks',         [TaskController::class, 'index']);
-        Route::post('/tasks',        [TaskController::class, 'store']);
-        Route::get('/tasks/{id}',    [TaskController::class, 'show']);
-        Route::put('/tasks/{id}',    [TaskController::class, 'update']);
-        Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+        Route::get('/tasks',         [TaskController::class, 'index'])->name('index');
+        Route::post('/tasks',        [TaskController::class, 'store'])->name('store');
+        Route::get('/tasks/{id}',    [TaskController::class, 'show'])->name('show');
+        Route::put('/tasks/{id}',    [TaskController::class, 'update'])->name('update');
+        Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('destroy');
     });
 });
